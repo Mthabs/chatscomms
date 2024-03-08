56d170fdccdb52e1226ab15ca23f0b0272866e42
Mthabs <mthabs1@gmail.com>
2024-03-08 11:51:58 +0000

    deployement commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch} from "react-router-dom";
+import Home from "./pages/home/home";
+import Videos from "./pages/videos/videos";
+import Photos from "./pages/photos/photos";
+import NotFound from "./pages/not-found/not-found";
+import "./styles/global.css"
+import Login from "./pages/login/login";
+import {AuthProvider} from "./Contexts/AuthContext";
+import Friends from "./pages/friends/friends";
+import Profile from "./pages/profile/profile";
+import SignUp from "./pages/sign-up/sign-up";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Switch>
+                <Route exact path="/" render={() => (
+                    <AuthProvider>
+                        <Login/>
+                    </AuthProvider>
+                )}/>
+                <Route exact path="/signup" render={() => (
+                    <AuthProvider>
+                        <SignUp/>
+                    </AuthProvider>
+                )}/>
+                <Route exact path="/feed/home" render={() => (
+                    <AuthProvider>
+                        <Home/>
+                    </AuthProvider>
+                )}/>
+                <Route path="/feed/videos" render={() => (
+                    <AuthProvider>
+                        <Videos/>
+                    </AuthProvider>
+                )}/>
+                <Route path="/feed/photos" render={() => (
+                    <AuthProvider>
+                        <Photos/>
+                    </AuthProvider>
+                )}/>
+                <Route path="/feed/friends" render={() => (
+                    <AuthProvider>
+                        <Friends/>
+                    </AuthProvider>
+                )}/>
+                <Route path="/feed/profile" render={() => (
+                    <AuthProvider>
+                        <Profile/>
+                    </AuthProvider>
+                )}/>
+                <Route component={NotFound}/>
+            </Switch>
+        </>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
